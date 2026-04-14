c19f142c7bc6b4587858768079b26d11c37aa9e5
conraddeas <205364008+conraddeas@users.noreply.github.com>
2026-04-14 15:52:45 -0400

    chore: downgrade @astrojs/cloudflare to v12 to fix Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    imageService: 'passthrough',
-  }),
+  adapter: cloudflare(),
 });
